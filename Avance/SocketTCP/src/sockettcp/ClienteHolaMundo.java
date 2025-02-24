cecd04529660d162a2545c80efc3ceff6e4b4912
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2025-02-24 15:33:55 -0400

    feat(avance):agregar implementación de cliente y servidor TCP para comunicación básica, con un ejemplo

---
+++ Avance/SocketTCP/src/sockettcp/ClienteHolaMundo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sockettcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author Dell
+ */
+public class ClienteHolaMundo {
+     public static void main(String[] args) {
+        // TODO code application logic here
+         int port = 5002;
+        try {
+            Socket client = new Socket("localhost", port);
+            PrintStream toServer = new PrintStream(client.getOutputStream());
+            BufferedReader fromServer = new BufferedReader(
+                    new InputStreamReader(client.getInputStream()));
+            toServer.println("Hola Mundo desde el Cliente");
+            String result = fromServer.readLine();
+            System.out.println("cadena devuelta por el servidor es: " + result);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
