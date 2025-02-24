5a98eed750fa1cc5c8e4bce926acd361c178f0b8
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2025-02-24 15:34:27 -0400

    ejercicio 1, invertir palabra cliente servidor SocketsTCP

---
+++ EjerciciosEnClases/EjercicioSocketsTCP/src/ejerciciosocketstcp/ClienteHolaMundo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosocketstcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dell
+ */
+public class ClienteHolaMundo {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+         int port = 5555;
+        try {
+            Socket client = new Socket("localhost", port);
+            PrintStream toServer = new PrintStream(client.getOutputStream());
+            BufferedReader fromServer = new BufferedReader(
+                    new InputStreamReader(client.getInputStream()));
+            //leemos la palabra
+            System.out.println("ingrese una palbra: ");
+            String palabra = sc.next();
+            //enviamos al servidor
+            toServer.println(palabra);
+            String result = fromServer.readLine();
+            System.out.println("cadena devuelta por el servidor es: " + result);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ EjerciciosEnClases/EjercicioSocketsTCP/src/ejerciciosocketstcp/ServerHolaMundo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosocketstcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Dell
+ */
+public class ServerHolaMundo {
+
+    public static String reverseInputString(String myString) {
+
+        if (myString == null) {
+            return myString;
+        }
+
+        String reverseString = "";
+
+        for (int i = myString.length() - 1; i >= 0; i--) {
+
+            reverseString = reverseString + myString.charAt(i);
+        }
+        return reverseString;
+    }
+
+    public static void main(String[] args) {
+        int port = 5555;
+        ServerSocket server;
+
+        try {
+            // TODO code application logic here
+            server = new ServerSocket(port);
+            System.out.println("Se inicio el servidor con éxito");
+            Socket client;
+            PrintStream toClient;
+            client = server.accept(); //conexion entre cliente y servidor para comunicacion bidireccional
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); // el lector
+            System.out.println("Cliente se conecto");
+            //recibimos la palabra
+            String recibido = fromClient.readLine();
+            System.out.println("El cliente envio el mensaje:" + recibido);
+            // invertimos la palabra
+            String invertido = reverseInputString(recibido);
+            toClient = new PrintStream(client.getOutputStream());
+            toClient.println("palabra invetida: " + invertido);
+        } catch (IOException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+
+}
